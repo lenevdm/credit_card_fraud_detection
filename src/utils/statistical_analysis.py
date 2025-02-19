c6feef727030bdc5174686c11b742964eeef0684
Lené van der Merwe <63034925+lenevdm@users.noreply.github.com>
2025-02-19 21:21:01 +0200

    Add stats utility

---
+++ src/utils/statistical_analysis.py
@@ -0,0 +1,14 @@
+""" Statistical analysis utilities for comparing detection techniques"""
+
+import numpy as np
+from scipy import stats
+from typing import Dict, List, Tuple, Optional, Union
+import pandas as pd
+
+def paired_t_test():
+    return
+
+
+def compare_techniques():
+    return
+
