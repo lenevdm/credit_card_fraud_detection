5b21726f3256c078fc13911b2fb53452a70cd5de
Lené van der Merwe <63034925+lenevdm@users.noreply.github.com>
2025-03-04 06:28:42 +0200

    Add ensemble experiment file

---
+++ src/experiments/ensemble_experiment.py
@@ -0,0 +1,11 @@
+""" Ensemble experiment implementation using probability averaging from multiple techniques"""
+
+class EnsembleExperiment(BaseExperiment):
+    """
+    
+    """
+
+    # def __init__
+
+
+    # def preprocess_data
